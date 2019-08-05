4590d5b3e1dc4a22514b5fc00f2197830970e156
Joe <rxyjoey@gmail.com>
2019-08-05 14:47:30 +0800

    add diff feature

--- portmon.py
+++ portmon.py
@@ -1,79 +1,116 @@
+import os
+import json
+import logging
+import threading
+import time
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import subprocess
+from pathlib import Path
 
 
-def get_res():
+# TODO read ports from conf file
+ports = [9999, 9998, 9997, 9996, 9995]
+usage_disk = {}
+last_usage = {}
+
+data_file = Path(os.path.join(os.getcwd(), 'data'))
+print(data_file)
+if not data_file.is_file():
+    with open('data', 'w') as fd:
+        data = {}
+        for p in ports:
+            data[p] = 0
+        fd.write(json.dumps(data))
+else:
+    with open(data_file) as fd:
+        usage_disk = json.load(fd)
+
+
+def get_iptable():
     output = subprocess.check_output(['iptables', '-L', '-v', '-n', '-x'])
     return output.decode("utf-8").splitlines()
 
 
 def add_ports_to_mon(unmoned_ports):
-    # TODO
-    pass
+    for p in unmoned_ports:
+        out = subprocess.check_output(['iptables', '-A', 'OUTPUT', '-p', 'tcp', '--sport', str(p)])
+        if out:
+            logging.error("add_ports_to_mon fail")
 
 
 def job():
-    ports = [9999, 9998, 9997, 9996, 9995]
-    res = get_res()
+    while True:
+        table = get_iptable()
 
-    # parse
-    first = 0
-    for i, line in enumerate(res):
-        if line.startswith("Chain OUTPUT"):
-            first = i + 2
+        first = 0
+        for i, line in enumerate(table):
+            if line.startswith("Chain OUTPUT"):
+                first = i + 2
 
-    ofs = 0
-    full_outputs_list = []
-    while res[first + ofs].strip():
-        full_outputs_list.append(res[first + ofs])
-        ofs += 1
+        ofs = 0
+        full_outputs_list = []
+        while table[first + ofs].strip():
+            full_outputs_list.append(table[first + ofs])
+            ofs += 1
 
-    outputs_list = []
-    moned_ports = []
-    for e in full_outputs_list:
-        for p in ports:
-            if "spt:" + str(p) in e:
-                outputs_list.append(e)
-                moned_ports.append(p)
-    unmoned_ports = set(ports) - set(moned_ports)
-    add_ports_to_mon(unmoned_ports)
+        moned_list = []
+        moned_ports = []
+        for e in full_outputs_list:
+            for p in ports:
+                if "spt:" + str(p) in e:
+                    moned_list.append(e)
+                    moned_ports.append(p)
+        unmoned_ports = set(ports) - set(moned_ports)
+        add_ports_to_mon(unmoned_ports)
 
-    usage = {}
-    for o in outputs_list:
-        so = o.split()
-        out = so[1]
-        if out[-1] == 'K':
-            out = out[:-1]
-        port = so[-1][4:]
-        usage[port] = out
-    print(usage)
+        usage = {}
+        for o in moned_list:
+            so = o.split()
+            out = so[1]
+            port = so[-1][4:]
+            usage[port] = int(out)
+
+        # global last_usage
+        for port, out in usage.items():
+            diff = out - last_usage.get(port, 0)
+            usage_disk[port] += diff
+            last_usage[port] = out
+
+        print(usage_disk)
+        # flush disk
+
+        # with open(filename, 'w') as out:
+        #     out.write(var + '\n')
+        time.sleep(2)
 
 
-    kb = 1024
-    gb = round(kb / 1024 / 1024, 3)
+def get_statistic(port):
+    b = usage_disk[port]
+    kb = int(b / 1024)
+    gb = round(kb / 1024 / 1024, 2)
     rmb = gb
-    return "Data usage: {}KB = {}GB => Bill: {}RMB".format(kb, gb, rmb)
+    return "Port {} data usage: {}KB = {}GB => Bill: {}RMB".format(port, kb, gb, rmb)
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         paths = {
+            '/': {'status': 200},
             '/9999': {'status': 200},
             '/9998': {'status': 200},
             '/9997': {'status': 200},
             '/9996': {'status': 200}
         }
-
         if self.path in paths:
             self.send_response(200)
             self.end_headers()
-            p = self.path
-            r = "Hello, {}. {}".format(p, job())
-            self.wfile.write(r.encode('utf-8'))
+            self.wfile.write(get_statistic(self.path[1:]).encode('utf-8'))
         else:
             self.send_response(404)
 
 
+jobt = threading.Thread(target=job, name='TrafficMonitorThread')
+jobt.start()
 httpd = HTTPServer(('0.0.0.0', 9000), SimpleHTTPRequestHandler)
 httpd.serve_forever()
