0398b504e53ea988d23cc7f82cab713f8fa069b1
Joe <rxyjoey@gmail.com>
2019-08-06 14:54:14 +0800

    lost

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 mkdir -p ~/.portmon
 cp portmon.service /lib/systemd/system/portmon.service
 cp portmon.py /usr/bin/portmon.py
+cp portmon.ini ~/.portmon/portmon.ini
 systemctl start portmon.service
 systemctl enabled portmon.service
