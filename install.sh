6a8f400929ef4935ca12fe82b3f96fcc56029557
Joe <rxyjoey@gmail.com>
2019-08-06 15:21:47 +0800

    fix install issue

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ mkdir -p ~/.portmon
 cp portmon.service /lib/systemd/system/portmon.service
 cp portmon.py /usr/bin/portmon.py
 cp portmon.ini ~/.portmon/portmon.ini
-systemctl start portmon.service
-systemctl enabled portmon.service
+systemctl enable --now portmon.service
