6ad4af17aaaeadcdfbadae593cd755a2d0ef8bbb
Joe <rxyjoey@gmail.com>
2019-08-06 11:58:43 +0800

    fix install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mkdir ~/.portmon
+mkdir -p ~/.portmon
 cp portmon.service /lib/systemd/system/portmon.service
 cp portmon.py /usr/bin/portmon.py
+systemctl start portmon.service
 systemctl enabled portmon.service
