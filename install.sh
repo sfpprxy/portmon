5efc196f561a31708bee86f969113b68502fad13
Joe <rxyjoey@gmail.com>
2020-07-27 09:43:21 +0800

    do not overwrite old config when install

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ mkdir -p ~/.portmon
 cp portmon.service /lib/systemd/system/portmon.service
 cp portmon.py /usr/bin/portmon.py
 cp -R bottle/ /usr/bin/
-cp portmon.ini ~/.portmon/portmon.ini
+cp -n portmon.ini ~/.portmon/portmon.ini
 systemctl enable --now portmon.service
